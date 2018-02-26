0ab3e4f91837a0e56ad671b863296ebab4b67bf0
Tarun Kumar <30823291+Tarunhacman@users.noreply.github.com>
2018-02-26 23:42:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //change the text of main-text
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+
+//Move the image modi
+var element=document.getElementById('modi');
+modi.onclick=function() {
+    modi.style.marginleft='100px';
+};
