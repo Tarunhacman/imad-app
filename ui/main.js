9e74cea39efba90cc2f5630a6e63e704b0f6b440
Tarun Kumar <30823291+Tarunhacman@users.noreply.github.com>
2018-03-01 00:33:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,6 @@
-console.log('Loaded!');
-//change the text of main-text
-var element=document.getElementById('main-text');
-element.innerHTML='New Value';
-
-//Move the image modi
-var element=document.getElementById('modi');
-img.onclick=function() {
-    img.style.marginLeft='300px';
-};
+//counter code
+var button= document.getElementById('counter');
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+}
