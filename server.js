88860b539a46a1635faa0949643ad908f09b1a02
Tarun Kumar <30823291+Tarunhacman@users.noreply.github.com>
2018-03-01 00:23:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ return createTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var counter=0;
 app.get('/counter',function (req,res){
     counter=counter+1;
     res.send(counter.toString());
